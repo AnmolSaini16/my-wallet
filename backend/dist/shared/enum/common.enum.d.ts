d242673bddf095c8eea1982ec74540c008f00564
AnmolSaini16 <sainianmol16@gmail.com>
2024-04-04 19:26:41 +0530

    TRying without dist deployment

--- backend/dist/shared/enum/common.enum.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum ErrorMessage {
-    BankNotFoundMsg = "Bank not found with specified account",
-    InvalidTransaction = "No transaction found with specified account"
-}
-export declare enum BankBalanceUpdateType {
-    Increment = "Increment",
-    Decrement = "Decrement"
-}
